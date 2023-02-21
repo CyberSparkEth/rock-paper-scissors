10f3d94f6bbd076636b2cfb8da5f5bb6884f893f
CyberSparkEth <prusovman@hotmail.com>
2023-02-21 23:10:23 +0400

    Initial commit

---
+++ RockPaperScissors.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
+
+contract RockPaperScissors is VRFV2WrapperConsumerBase {
+
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+
+    address owner;
+
+    enum Choice { None, Rock, Paper, Scissors }
+    enum Outcome { Tie, PlayerWins, BotWins }
+
+    event GameCreated(uint256 gameId, address player, uint256 bet);
+    event GameCompleted(uint256 gameId, Outcome outcome);
+
+    struct RequestStatus {
+        uint256 paid;
+        bool fulfilled;
+        uint256[] randomWords;
+    }
+
+    struct Game {
+        address player;
+        uint256 bet;
+        Choice playerChoice;
+        Choice botChoice;
+        Outcome outcome;
+        bool completed;
+    }
+
+    mapping (address => uint256) public balances;
+    mapping (uint256 => Game) public games;
+    mapping(uint256 => RequestStatus)
+        public s_requests;
+
+    uint32 callbackGasLimit = 400000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+    address linkAddress = 0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06;
+    address wrapperAddress = 0x699d428ee890d55D56d5FC6e26290f3247A762bd;
+
+    constructor()
+        VRFV2WrapperConsumerBase(linkAddress, wrapperAddress)
+        payable{
+            owner = msg.sender;
+        }
+
+    function createGame(Choice choice) public payable {
+        require(msg.value > 0, "Bet amount must be greater than 0");
+        require(choice != Choice.None, "Invalid choice");
+        uint256 gameId = requestRandomness(callbackGasLimit, requestConfirmations, numWords);
+        games[gameId] = Game(msg.sender, msg.value, Choice.None, Choice.None, Outcome.Tie, false);
+        games[gameId].playerChoice = choice;
+        Choice botChoice = Choice(uint256(gameId) % 3 + 1);
+        games[gameId].botChoice = botChoice;
+        completeGame(gameId);
+        emit GameCreated(gameId, msg.sender, msg.value);
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].paid > 0, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+    }
+
+    function completeGame(uint256 gameId) internal {
+        require(gameId != 0, "Game ID not found");
+        Game storage game = games[gameId];
+        require(!game.completed, "Game is already completed");
+
+        if (game.playerChoice == game.botChoice) {
+            game.outcome = Outcome.Tie;
+            payable(msg.sender).transfer(game.bet);
+        } else if ((game.playerChoice == Choice.Rock && game.botChoice == Choice.Scissors) ||
+                (game.playerChoice == Choice.Paper && game.botChoice == Choice.Rock) ||
+                (game.playerChoice == Choice.Scissors && game.botChoice == Choice.Paper)) {
+            game.outcome = Outcome.PlayerWins;
+            payable(msg.sender).transfer(game.bet * 2);
+        } else {
+            game.outcome = Outcome.BotWins;
+        }
+
+        game.completed = true;
+        emit GameCompleted(gameId, game.outcome);
+    }
+
+    function depositBalance() public payable {
+        balances[address(this)] += msg.value;
+    }
+
+    function withdrawBalance() public onlyOwner {
+        require(address(this).balance > 0, "Insufficient balance");
+        payable(msg.sender).transfer(address(this).balance);
+    }
+    
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(linkAddress);
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
---
+++ script.js
@@ -0,0 +1,263 @@
+const contractAddress = "0x23Fd3fA2A87Aa61Be53C6c87418D41417B5F70c1";
+const contractABI = [
+	{
+		"inputs": [],
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "gameId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "enum RockPaperScissors.Outcome",
+				"name": "outcome",
+				"type": "uint8"
+			}
+		],
+		"name": "GameCompleted",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "gameId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "player",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "bet",
+				"type": "uint256"
+			}
+		],
+		"name": "GameCreated",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "enum RockPaperScissors.Choice",
+				"name": "choice",
+				"type": "uint8"
+			}
+		],
+		"name": "createGame",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "depositBalance",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "games",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "player",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "bet",
+				"type": "uint256"
+			},
+			{
+				"internalType": "enum RockPaperScissors.Choice",
+				"name": "playerChoice",
+				"type": "uint8"
+			},
+			{
+				"internalType": "enum RockPaperScissors.Choice",
+				"name": "botChoice",
+				"type": "uint8"
+			},
+			{
+				"internalType": "enum RockPaperScissors.Outcome",
+				"name": "outcome",
+				"type": "uint8"
+			},
+			{
+				"internalType": "bool",
+				"name": "completed",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_requestId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "_randomWords",
+				"type": "uint256[]"
+			}
+		],
+		"name": "rawFulfillRandomWords",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "s_requests",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "paid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "fulfilled",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdrawBalance",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdrawLink",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, 97)//ChainID 97 BNBtestnet
+let signer;
+let contract;
+
+const event = "createGame";
+
+provider.send("eth_requestAccounts", []).then(()=>{
+    provider.listAccounts().then( (accounts) => {
+        signer = provider.getSigner(accounts[0]); //account in metamask
+        
+        contract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+        )
+     
+    }
+    )
+}
+)
+
+function choiceToString(choice) {
+    switch (choice) {
+      case 1:
+        return 'Rock 🗿';
+      case 2:
+        return 'Paper 📄';
+      case 3:
+        return 'Scissors ✂️';
+      default:
+        return 'None';
+    }
+  }
+
+function outcomeToString(outcome) {
+  switch (outcome) {
+    case 0:
+      return 'Tie';
+    case 1:
+      return 'You win 🎉';
+    case 2:
+      return 'You lose 😥';
+    default:
+      return 'None';
+  }
+}
+
+async function gameCreate() {
+    let choice = document.getElementById('choice').value;
+    console.log(choice);
+    let betInEth = document.getElementById('bet').value;
+    let betInWei = ethers.utils.parseEther(betInEth.toString());
+    console.log(betInWei);
+    
+    await contract.createGame(choice, {value: betInWei});
+    await contract.on('GameCreated', (gameId) => {
+        contract.games(gameId.toString()).then(result => {
+            const [player, bet, playerChoice,
+                 botChoice, outcome, completed] = result;
+
+    let resultLogs = `
+    bet: ${ethers.utils.formatEther(bet.toString())} BNB,
+    player choice: ${choiceToString(playerChoice)},
+    bot choice: ${choiceToString(botChoice)},
+    result: ${outcomeToString(outcome)}`
+
+    alert(resultLogs)
+    });
+    
+    });
+}
